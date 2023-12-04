05776e8794f49179ae6f6389b96be94b979d6722
Tarashrita-Ganguly <150145692+Tarashrita-Ganguly@users.noreply.github.com>
2023-12-04 19:22:01 +0530

    Add files via upload

--- Bear/Bear.js
+++ Bear/Bear.js
@@ -1,18 +1,34 @@
+status= "";
 function preload() {
-    img=loadImage('dog_cat.jpg');
+    img=loadImage('Bear.jpg');
 }
 
 function setup() {
-canvas=createCanvas(640,420);
+canvas=createCanvas(640,640);
 canvas.center();
-
+objectDetector=ml5.objectDetector('cocossd',ModelLoaded);
+document.getElementById("status").innerHTML="Status: Detecting object";
 }
 
 function draw() {
-    image(img,0,0,640,420);
+    image(img,0,0,640,640);
     fill("#FF0000");
     text("dog",45,75);
     noFill();
     stroke("#FF0000");
-    rect(30,60,450,350);
+    rect(30,60,610,610);
+}
+
+function ModelLoaded() {
+    console.log("model loaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results) {
+if(error) {
+  console.log(error);
+}else{
+    console.log(results);
+}
 }
