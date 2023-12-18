b6ad593b43f4855e12d4f6289f4f2f05ab0da31d
Tarashrita-Ganguly <150145692+Tarashrita-Ganguly@users.noreply.github.com>
2023-12-18 21:09:55 +0530

    Add files via upload

--- Bear/Bear.js
+++ Bear/Bear.js
@@ -1,34 +1,50 @@
-status= "";
-function preload() {
-    img=loadImage('Bear.jpg');
+objectDetector= "";
+
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+  img = loadImage('Bear.jpg');
 }
+
 
 function setup() {
-canvas=createCanvas(640,640);
-canvas.center();
-objectDetector=ml5.objectDetector('cocossd',ModelLoaded);
-document.getElementById("status").innerHTML="Status: Detecting object";
+  canvas = createCanvas(640, 420);
+  canvas.center();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
 
 function draw() {
-    image(img,0,0,640,640);
-    fill("#FF0000");
-    text("dog",45,75);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,610,610);
-}
+  image(img, 0, 0, 640, 420);
 
-function ModelLoaded() {
-    console.log("model loaded");
-    status=true;
-    objectDetector.detect(img,gotResult);
-}
-
-function gotResult(error,results) {
-if(error) {
-  console.log(error);
-}else{
-    console.log(results);
-}
+      if(status != "")
+      {
+        for (var i = 0; i < objects.length; i++) {
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+    
+          fill(255, 0, 0);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+          noFill();
+          stroke(255, 0, 0);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+      }
 }
